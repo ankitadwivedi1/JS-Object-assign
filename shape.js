60c0f5427adf0489971f0650f0d79fa8de6c88ba
ankitadwivedi1 <60252894+ankitadwivedi1@users.noreply.github.com>
2021-12-07 00:17:00 +0530

    Add files via upload

---
+++ shape.js
@@ -0,0 +1,16 @@
+
+class Shape{
+    constructor(color){
+     this.color = color;
+    }
+    drawShape(){
+     console.log("Drawing Shape");
+    }
+    calculateArea(){
+     console.log("Calculating area of your shape")
+    }
+   }
+   module.exports = Shape;
+   © 2021 GitHub, Inc.
+   Terms
+   Priv
